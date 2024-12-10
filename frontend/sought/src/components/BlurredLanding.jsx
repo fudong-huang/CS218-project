fb0a5dbbe87beba6ece1970352198a35c9d85219
zixiao <fanzixiao9@gmail.com>
2024-12-09 17:28:28 -0800

    Update BlurredLanding.jsx

--- frontend/sought/src/components/BlurredLanding.jsx
+++ frontend/sought/src/components/BlurredLanding.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { useNavigate } from 'react-router-dom'
 import { ArrowRight, Search, Bell, Settings, 
          Calendar, MessageSquare, Shield, Home, 
